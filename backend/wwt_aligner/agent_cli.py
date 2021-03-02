e9dc92fb3a6bdb2fbfdd9dc0a6b140cc58bf7c07
Peter Williams <peter@newton.cx>
2021-03-01 21:10:07 -0500

    Baseline implementation of args protocol between Docker and frontend

--- backend/wwt_aligner/agent_cli.py
+++ backend/wwt_aligner/agent_cli.py
@@ -1,10 +1,13 @@
 # Copyright 2020 the .NET Foundation
 # Licensed under the MIT License.
 
-"""Entrypoint for the "wwt-aligner" command-line interface.
+"""
+Entrypoint for the "wwt-aligner[-agent]" command-line interface.
 
 """
 import argparse
+import json
+import os.path
 import shutil
 import sys
 import tempfile
@@ -20,6 +23,99 @@ def die(msg):
 
 def warn(msg):
     print('warning:', msg, file=sys.stderr)
+
+
+# The agent is generally expected to run inside a Docker container. In order to
+# be able to do its input and output, then, its container needs to be set up
+# with the appropriate filesystem mounts by the program that launches it. So
+# there needs to be some kind of analysis of the program arguments and
+# identification of which directories need to be mounted on the launcher side.
+#
+# But as long as we're running the agent inside Docker, it is/would be *really*
+# nice to be able to get new CLI commands just by updating the container. We
+# could ask people to update the launcher but would prefer if it didn't have to
+# stay in close sync with the containerized app. You could imagine a self-update
+# scheme where we update the launcher with a binary stored inside the container,
+# but that's a bit tricky to implement.
+#
+# Instead, we the launcher run each command twice, the first time passing in a
+# special hidden `--x-analyze-args-mode` flag. When this flag is active, the agent
+# returns JSON specifying what kind of environment setup the launcher needs to
+# perform. The agent then runs the command "for real" as instructed. This way
+# the launcher can start handling new commands without needing to explicitly
+# know about them. This handshake is the "args protocol".
+
+ARGS_PROTOCOL_VERSION = 1
+
+class ArgPiece(object):
+    text = None
+    "The text of this argument piece."
+
+    incomplete = False
+    """If true, the subsequent arg piece should be concatenated to this one
+    without a space. This allows us to handle arguments of the form
+    `--path=./somepath.txt`."""
+
+    path_pre_exists = False
+    """If true, this piece is a filesystem path and it should exist before the
+    program starts running."""
+
+    path_created = False
+    """If true, this piece is a filesystem path that the program will create
+    during its exection. Therefore its containing directory should be mounted
+    read-write in the Docker container."""
+
+    def __init__(self, text, incomplete=False, path_pre_exists=False, path_created=False):
+        self.text = str(text)
+        self.incomplete = incomplete
+        self.path_pre_exists = path_pre_exists
+        self.path_created = path_created
+
+    def as_json(self):
+        data = {'text': self.text}
+
+        if self.incomplete:
+            data['incomplete'] = True
+
+        if self.path_pre_exists:
+            data['path_pre_exists'] = True
+
+        if self.path_created:
+            data['path_created'] = True
+
+        return data
+
+
+class ArgsProtocolBuilder(object):
+    pieces = None
+
+    def __init__(self):
+        self.pieces = []
+
+    def add_arg(self, arg, incomplete=False):
+        """
+        Add an argument that needs no translation from the host to the container.
+        """
+        self.pieces.append(ArgPiece(arg, incomplete=incomplete))
+
+    def add_path_arg(self, path, incomplete=False, pre_exists=False, created=False):
+        """
+        Add an argument that is a path on the host.
+        """
+        assert pre_exists or created
+        self.pieces.append(ArgPiece(
+            path,
+            incomplete = incomplete,
+            path_pre_exists = pre_exists,
+            path_created = created,
+        ))
+
+    def write_as_json(self, fp):
+        data = {
+            'version': ARGS_PROTOCOL_VERSION,
+            'pieces': [p.as_json() for p in self.pieces],
+        }
+        json.dump(data, fp, ensure_ascii=False, indent=2, sort_keys=True)
 
 
 # "go" subcommand
@@ -40,6 +136,14 @@ def go_getparser(parser):
         metavar = 'RGB-PATH',
         help = 'The path to input file to solve',
     )
+
+
+def go_analyze_args(builder, settings):
+    if settings.anet_bin_prefix:
+        builder.add_arg(f'--anet-bin-prefix={settings.anet_bin_prefix}')
+
+    builder.add_path_arg(settings.fits_path, pre_exists=True)
+    builder.add_path_arg(settings.rgb_path, pre_exists=True)
 
 
 def go_impl(settings):
@@ -71,6 +175,24 @@ def entrypoint(args=None):
     # Set up the subcommands from globals()
 
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--x-analyze-args-mode',
+        dest = 'analyze_args_mode',
+        action = 'store_true',
+        help = argparse.SUPPRESS,
+    )
+    parser.add_argument(
+        '--x-host-path',
+        dest = 'host_paths',
+        action = 'append',
+        help = argparse.SUPPRESS,
+    )
+    parser.add_argument(
+        '--x-container-path',
+        dest = 'container_paths',
+        action = 'append',
+        help = argparse.SUPPRESS,
+    )
     subparsers = parser.add_subparsers(dest="subcommand")
     commands = set()
 
@@ -94,10 +216,21 @@ def entrypoint(args=None):
 
     py_name = settings.subcommand.replace('-', '_')
 
-    impl = globals().get(py_name + '_impl')
-    if impl is None:
-        die('no such subcommand "{}"'.format(settings.subcommand))
+    if not settings.analyze_args_mode:
+        # Just Do It.
+        impl = globals().get(py_name + '_impl')
+        if impl is None:
+            die('no such subcommand "{}"'.format(settings.subcommand))
 
-    # OK to go!
+        impl(settings)
+    else:
+        # We're in args-analysis mode.
+        aa = globals().get(py_name + '_analyze_args')
+        if aa is None:
+            die('no such (analyzable) subcommand "{}"'.format(settings.subcommand))
 
-    impl(settings)
+        builder = ArgsProtocolBuilder()
+        builder.add_arg(settings.subcommand)
+        aa(builder, settings)
+        builder.write_as_json(sys.stdout)
+        print()
--- frontend/src/docker.rs
+++ frontend/src/docker.rs
@@ -1,12 +1,15 @@
-// Copyright 2020 the .NET Foundation
+// Copyright 2020-2021 the .NET Foundation
 // Licensed under the MIT License
 
 //! Tools for driving Docker.
 
+use anyhow::ensure;
+use serde::Deserialize;
 use std::{
     collections::HashMap,
     ffi::{OsStr, OsString},
-    path::{Path, PathBuf},
+    io::{self, Write},
+    path::PathBuf,
     process::Command,
 };
 
@@ -14,23 +17,29 @@ use crate::{a_ok_or, atry, errors::Result};
 
 const DOCKER_COMMAND: &str = "docker";
 const DEFAULT_IMAGE_NAME: &str = "aasworldwidetelescope/aligner:latest";
+const DEFAULT_INNER_COMMAND: &str = "wwt-aligner-agent";
+const SUPPORTED_ARGS_PROTOCOL_VERSION: usize = 1;
 
 /// Helper for constructing Docker command lines.
 #[derive(Debug)]
 pub struct DockerBuilder {
     image_name: String,
-
-    /// Host dirname to in-Docker dirname.
-    host_dirs: HashMap<PathBuf, PathBuf>,
-
+    volumes: Vec<DockerVolume>,
     inner_args: Vec<OsString>,
+}
+
+#[derive(Debug)]
+struct DockerVolume {
+    host_path: PathBuf,
+    container_path: PathBuf,
+    read_write: bool,
 }
 
 impl Default for DockerBuilder {
     fn default() -> Self {
         DockerBuilder {
             image_name: DEFAULT_IMAGE_NAME.to_owned(),
-            host_dirs: Default::default(),
+            volumes: Default::default(),
             inner_args: Default::default(),
         }
     }
@@ -42,51 +51,231 @@ impl DockerBuilder {
         self
     }
 
-    pub fn file_arg<P: AsRef<Path>>(&mut self, host_path: P) -> Result<&mut Self> {
-        let host_path = host_path.as_ref();
-        let host_canon = atry!(
-            host_path.canonicalize();
-            ["could not determine canonical path for input argument `{}`", host_path.display()]
-        );
-        let host_dirname = a_ok_or!(
-            host_canon.parent();
-            ["cannot convey root-type input path `{}` to Docker", host_path.display()]
+    pub fn for_analyzed_command(args: &[OsString]) -> Result<Option<Self>> {
+        let mut analyze_cmd = Command::new(DOCKER_COMMAND);
+        analyze_cmd
+            .arg("run")
+            .arg("--rm")
+            .arg(DEFAULT_IMAGE_NAME)
+            .arg(DEFAULT_INNER_COMMAND)
+            .arg("--x-analyze-args-mode")
+            .args(args);
+
+        let output = atry!(
+            analyze_cmd.output();
+            ["failed to launch the Docker command: {:?}", analyze_cmd]
         );
 
-        let cnt_dirname = self
-            .host_dirs
-            .entry(host_dirname.to_owned())
-            .or_insert_with(|| {
-                let mut container_dir = PathBuf::new();
-                container_dir.push("/hostdirs");
-                let c = host_dirname.display().to_string().replace("/", "_");
-                container_dir.push(c);
-                container_dir
-            });
+        // If there was any stderr output, get it out there.
+        atry!(
+            io::stderr().write_all(&output.stderr);
+            ["failed to transfer Docker error output to stderr"]
+        );
 
-        // Note that the file name has to come from the canonicalized path, in
-        // case it was a symlink to a file with a different actual name.
-        let mut cnt_path = cnt_dirname.clone();
-        if let Some(n) = host_canon.file_name() {
-            cnt_path.push(n);
+        if let Some(0) = output.status.code() {
+        } else {
+            if let Some(c) = output.status.code() {
+                if c == 2 {
+                    // Signifies a failure to parse arguments according to
+                    // Python argparse's defaults. In that case, the usage
+                    // message will have already been written to stderr, so
+                    // return a value indicating failure that has already
+                    // been reported.
+                    return Ok(None);
+                }
+
+                // Otherwise, something went unexpectedly wrong.
+
+                eprintln!(
+                    "error: the Docker command signaled failure (error code {})",
+                    c
+                );
+            } else {
+                eprintln!("error: the Docker command exited unexpectedly");
+            }
+
+            if !output.stdout.is_empty() {
+                eprintln!("error: the command's primary output was:\n");
+                atry!(
+                    io::stderr().write_all(&output.stdout);
+                    ["failed to transfer Docker stdout output"]
+                );
+            }
         }
 
-        self.arg(cnt_path);
-        Ok(self)
+        // OK, the command seems to have succeeded. Get the arg info from its
+        // output.
+
+        // TODO: recognize `--help` mode
+
+        let mut apdata: ArgsProtocolData = atry!(
+            serde_json::from_slice(&output.stdout).map_err(|e| {
+                eprintln!("error: failed to parse the following args-analysis output:\n\n");
+                let _r = io::stderr().write_all(&output.stdout);
+                e
+            });
+            ["internal error: failed to parse args-analysis output as JSON text"]
+        );
+
+        ensure!(
+            apdata.version <= SUPPORTED_ARGS_PROTOCOL_VERSION,
+            "you must upgrade the launcher in order to use this version of the aligner"
+        );
+
+        // Finally, create our actual Docker command from the analyzed arguments.
+
+        let mut builder = DockerBuilder::default();
+        let mut volumes = HashMap::new();
+        let mut arg = String::new();
+        let mut args = Vec::new();
+
+        builder.arg(DEFAULT_INNER_COMMAND);
+
+        for piece in apdata.pieces.drain(..) {
+            let is_path = piece.path_pre_exists || piece.path_created;
+
+            let processed_piece = if !is_path {
+                piece.text
+            } else {
+                // A path argument. We've got to prep to mount this path's
+                // containing directory as a volume inside the Docker container,
+                // with remapping.
+                //
+                // First, we need to make sure to transform this into a
+                // canonical (fully resolved) host path, because if the path
+                // contains a symlink the Docker volume mapping can fail in all
+                // sorts of ways. But even this step is tricky, because the
+                // canonicalization step will fail if the path does not exist,
+                // which will be the case for output files.
+
+                let host_path = if piece.path_pre_exists {
+                    // This path should exist. We can (must) use std::fs::canonicalize
+                    // (in case the final path component is a symlink).
+                    atry!(
+                        std::fs::canonicalize(&piece.text);
+                        ["failed to resolve filesystem path `{}`", &piece.text]
+                    )
+                } else {
+                    // The path will be created. We'll assume that it will be
+                    // blown away if it already exists, so we don't bother to
+                    // test if it does. But we also assume that its immediate
+                    // containing directory exists.
+
+                    let mut path_pieces = piece.text.rsplitn(2, std::path::is_separator);
+                    let basename = a_ok_or!(
+                        path_pieces.next();
+                        ["cannot process empty input path `{}`", &piece.text]
+                    );
+                    println!("BASENAME: {:?}", basename);
+                    let dirname = path_pieces.next().unwrap_or(".");
+                    println!("DIRNAME: {:?}", dirname);
+
+                    let mut canon = atry!(
+                        std::fs::canonicalize(&dirname);
+                        ["failed to resolve directory of output path `{}`", &piece.text]
+                    );
+
+                    canon.push(basename);
+                    canon
+                };
+
+                // OK, now that we have the canonical host path, we can determine its
+                // containing directory.
+
+                let mut host_dir = host_path.clone();
+                host_dir.pop();
+
+                // Make up a container path for this host path.
+
+                let container_dir = format!(
+                    "/volumes/{}",
+                    host_dir.to_string_lossy().replace(
+                        |c: char| !c.is_alphanumeric() || std::path::is_separator(c),
+                        "_"
+                    )
+                );
+
+                let host_fn = a_ok_or!(
+                    host_path.file_name();
+                    ["cannot process path `{}`, which lacks a filename component", &piece.text]
+                );
+
+                let host_fn = a_ok_or!(
+                    host_fn.to_str();
+                    ["cannot handle path `{}`, which resolves to something inexpressible as Unicode", &piece.text]
+                );
+
+                let container_path = format!("{}/{}", container_dir, host_fn);
+
+                // Add secret hidden arguments enabling the agent to pretend
+                // that its internal container paths are host paths.
+
+                builder.arg("--x-host-path");
+                builder.arg(piece.text);
+                builder.arg("--x-container-path");
+                builder.arg(&container_path);
+
+                // Ensure that we will have a Docker volume mount so that this
+                // file can be accessed inside the container.
+
+                let vol = volumes.entry(host_dir.clone()).or_insert(DockerVolume {
+                    host_path: host_dir,
+                    container_path: container_dir.into(),
+                    read_write: false,
+                });
+
+                if piece.path_created {
+                    vol.read_write = true;
+                }
+
+                // Finally, the ultimate "processed" value of this argument to propagate
+                // into the docker container:
+                container_path
+            };
+
+            // We can finally actually deal with this as an argument.
+
+            arg.push_str(&processed_piece);
+
+            if !piece.incomplete {
+                args.push(arg.clone());
+                arg.clear();
+            }
+        }
+
+        if !arg.is_empty() {
+            args.push(arg);
+        }
+
+        // We have handled all of the arg pieces! Now that all of the magic
+        // `--x-*-path` args are in place, we can add the real args.
+
+        for arg in args.drain(..) {
+            builder.arg(arg);
+        }
+
+        // Deal with the volumes and we're done.
+
+        for (_, vi) in volumes.drain() {
+            builder.volumes.push(vi);
+        }
+
+        Ok(Some(builder))
     }
 
     pub fn into_command(mut self) -> Command {
         let mut cmd = Command::new(DOCKER_COMMAND);
 
-        cmd.arg("run").arg("--rm");
+        cmd.arg("run").arg("--rm").arg("-it");
 
-        for (host_path, cnt_path) in self.host_dirs.drain() {
+        for vol in self.volumes.drain(..) {
             cmd.arg("-v");
 
-            let mut vstr = OsString::from(host_path);
+            let mut vstr = OsString::from(vol.host_path);
             vstr.push(":");
-            vstr.push(cnt_path);
-            vstr.push(":rw,z");
+            vstr.push(vol.container_path);
+            vstr.push(":");
+            vstr.push(if vol.read_write { "rw" } else { "ro" });
             cmd.arg(vstr);
         }
 
@@ -96,6 +285,7 @@ impl DockerBuilder {
             cmd.arg(arg);
         }
 
+        println!("QQQ {:?}", cmd);
         cmd
     }
 }
@@ -105,4 +295,35 @@ pub fn update_command() -> Command {
     let mut cmd = Command::new(DOCKER_COMMAND);
     cmd.arg("pull").arg(DEFAULT_IMAGE_NAME);
     cmd
+}
+
+/// The main "args protocol" data payload.
+#[derive(Debug, Deserialize)]
+struct ArgsProtocolData {
+    version: usize,
+    pieces: Vec<ArgsProtocolPieceInfo>,
+}
+
+/// Information about a volume to be mounted in the Docker container.
+#[derive(Debug, Deserialize)]
+struct ArgsProtocolPieceInfo {
+    /// The argument text.
+    text: String,
+
+    /// If true, there should be a subsequent argument piece that will be
+    /// concatenated to this one without being broken into a separate argument,
+    /// and without any whitespace being added.
+    #[serde(default)]
+    incomplete: bool,
+
+    /// If true, this piece corresponds to a filesystem path on the host, that
+    /// should exist before the tool runs.
+    #[serde(default)]
+    path_pre_exists: bool,
+
+    /// If true, this piece corresponds to a filesystem path on the host, and it
+    /// will be created by the tool. Therefore the containing directory of this
+    /// path should be mounted read-write inside the container.
+    #[serde(default)]
+    path_created: bool,
 }
